b3880133641473997dc690b5aea5a29651628862
Tumilara Adetayo <adetumilara.adetayo@stu.cu.edu.ng>
2026-02-22 13:44:38 +0100

    fox build issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import Activity from './app/dashboard/Activity';
 import Analytics from './app/dashboard/Analytics';
 import Settings from './app/dashboard/Settings';
 import Templates from './app/dashboard/Templates';
-import ToastDemo from './components/ToastDemo';
 
 function App() {
   return (
